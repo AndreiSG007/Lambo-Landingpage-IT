d6a684e3be4f99a93cbb6a8426892f5a9ba44364
AndreiSGOO7 <andrei@softgenius.com>
2023-02-16 11:15:15 +0200

    add config

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="test">test</div>
+</template>
+
+<style scoped lang="scss">
+.test {
+  color: red;
+  &:hover {
+    color: blue;
+  }
+}
+</style>
